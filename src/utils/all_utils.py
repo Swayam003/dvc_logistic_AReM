5f551ee2e341b5ab4766c65761040dce77df20d9
Swayam003 <swayam.roxx@gmail.com>
2021-10-14 22:09:23 +0530

    util file updated

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -3,11 +3,19 @@ import os
 import logging
 
 def read_yaml(path_to_yaml: str) -> dict:
+    """
+    It reads a path to yaml file and returns a dict
+    :param path_to_yaml:str
+    """
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
     return content
 
 def create_directory(dirs: list):
+    """
+    It is used to create a directory
+    :param dirs: list
+    """
     try:
         for dir_path in dirs:
             os.makedirs(dir_path, exist_ok=True)
