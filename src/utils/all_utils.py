a1b3fcda9bfc0dbf465104e0e25017a1efc95125
Swayam003 <swayam.roxx@gmail.com>
2021-10-15 01:57:16 +0530

    util file updated

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -6,10 +6,15 @@ def read_yaml(path_to_yaml: str) -> dict:
     """
     It reads a path to yaml file and returns a dict
     :param path_to_yaml:str
+    :return dict:dict
     """
-    with open(path_to_yaml) as yaml_file:
-        content = yaml.safe_load(yaml_file)
-    return content
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+        return content
+    except Exception as e:
+        logging.exception("error while loading yaml file")
+        raise e
 
 def create_directory(dirs: list):
     """
@@ -23,4 +28,18 @@ def create_directory(dirs: list):
             print(f"directory is created at {dir_path}")
     except Exception as e:
         logging.exception("error while creating directory:   ", e)
+        raise e
+
+def save_local_df(data, data_path, index_status=False):
+    """
+    This function saves the dataframe to CSV File
+    :param data: dataframe
+    :param data_path: path to save dataframe
+    :param index_status: default: False
+    """
+    try:
+        data.to_csv(data_path, index=index_status)
+        logging.info(f"data is saved at {data_path}")
+    except Exception as e:
+        logging.exception("error while saving dataframe to csv file: ", e)
         raise e
